b20497994ff56f4ef601ce4ae8b0fd8a3c1e6817
Michel Mattos <mattos.michel@gmail.com>
2015-11-16 01:37:44 -0200

    Adicionado tempo de espera no inicio dos testes e2e (tentando resolver o problema com o SauceLabs)

--- config/protractor.js
+++ config/protractor.js
@@ -13,18 +13,17 @@ exports.config = {
 	specs: ['../test/e2e/**/*.js'],
 
 	onPrepare: function() {
-		browser.driver.get('http://localhost:3000')
-			.then(function() {
-				browser.driver.findElement(by.id('entrar'))
-					.click();
-				// Login no GitHub
-				browser.driver.findElement(by.id('login_field'))
-					.sendKeys(config.seleniumUser);
-				browser.driver.findElement(by.id('password'))
-					.sendKeys(config.seleniumUserPassword);
-				browser.driver.findElement(by.name('commit'))
-					.click();
-			});
+		browser.driver.get('http://localhost:3000');
+		browser.driver.sleep(4000);
+		browser.driver.findElement(by.id('entrar'))
+			.click();
+		// Login no GitHub
+		browser.driver.findElement(by.id('login_field'))
+			.sendKeys(config.seleniumUser);
+		browser.driver.findElement(by.id('password'))
+			.sendKeys(config.seleniumUserPassword);
+		browser.driver.findElement(by.name('commit'))
+			.click();
 	}
 
 }
