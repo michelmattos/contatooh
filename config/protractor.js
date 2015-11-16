3c23dd980f49cf203c61abae2dd96a0c804d1656
Michel Mattos <mattos.michel@gmail.com>
2015-11-16 10:28:48 -0200

    Modificada a url de entrada

--- config/protractor.js
+++ config/protractor.js
@@ -14,7 +14,7 @@ exports.config = {
 	specs: ['../test/e2e/**/*.js'],
 
 	onPrepare: function() {
-		browser.driver.get('http://localhost:3000/');
+		browser.driver.get('http://localhost:3000/#/auth');
 		browser.driver.sleep(10000);
 		browser.driver.findElement(by.id('entrar'))
 			.click();
