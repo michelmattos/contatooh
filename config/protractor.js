c6daabc6ec7f505f8cc286f7ecbf6a92f0424faa
Michel Mattos <mattos.michel@gmail.com>
2015-11-16 17:05:58 -0200

    Update protractor.js

--- config/protractor.js
+++ config/protractor.js
@@ -1,7 +1,7 @@
 var config = require('./config')();
 
 exports.config = {
-	sauceSeleniumAddress: 'localhost:4445/wd/hub',
+	//sauceSeleniumAddress: 'localhost:4445/wd/hub',
 	sauceUser: config.sauceUser,
 	sauceKey: config.sauceKey,
 	capabilities: {
